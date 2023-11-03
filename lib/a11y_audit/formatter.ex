47ef38a41ff2e693583ba3fe194938e47a90f93c
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-11-03 16:44:34 +0100

    First version of formatting violations

--- lib/a11y_audit/formatter.ex
+++ lib/a11y_audit/formatter.ex
@@ -6,8 +6,8 @@ defmodule A11yAudit.Formatter do
   @doc false
   def format_result(result) do
     %A11yAudit.Results.Result{
-      id: id,
-      description: description,
+      id: _id,
+      description: _description,
       help: help,
       help_url: help_url,
       impact: impact,
@@ -16,11 +16,16 @@ defmodule A11yAudit.Formatter do
 
     color = impact_color(impact)
 
-    header = "#{with_color("#{reverse()} #{impact} ", color)} #{id}"
-    left_padding = with_color("┃ ", color)
+    header = "#{with_color("#{reverse()} #{impact} ", color)} #{help}"
+    left_padding = "  "
+
+    nodes_with_padding = format_nodes(nodes) |> Enum.map(fn line -> left_padding <> line end)
 
     """
-
+    #{header}
+    #{left_padding}Learn more: #{help_url}
+    #{left_padding}
+    #{nodes_with_padding}
     """
   end
 
@@ -36,18 +41,48 @@ defmodule A11yAudit.Formatter do
       nodes
       |> Enum.take(max_nodes_per_result)
       |> Enum.with_index()
-      |> Enum.map(fn {node, index} ->
-        """
-        Node #{index + 1}/#{total_nodes}
-        #{trim_string(node.html, max_node_html_length)}
-        """
+      |> Enum.flat_map(fn {node, index} ->
+        index_string = "#{index + 1}. "
+
+        [
+          "#{index_string}#{trim_string(node.html, max_node_html_length)}\n"
+        ] ++
+          if node.failure_summary do
+            String.split(
+              "\n" <> format_list_in_failure_summary(node.failure_summary) <> "\n\n",
+              ~r/(.*)\n/,
+              include_captures: true,
+              trim: true
+            )
+            |> Enum.map(fn string ->
+              if String.trim(string) == "" do
+                string
+              else
+                String.duplicate(" ", String.length(index_string)) <> string
+              end
+            end)
+          else
+            []
+          end
       end)
 
-    if max_nodes_per_result >= total_nodes do
-      formatted_nodes
-    else
-      formatted_nodes ++ ["... and #{total_nodes - max_nodes_per_result} more nodes.\n"]
-    end
+    formatted_nodes =
+      if max_nodes_per_result >= total_nodes do
+        formatted_nodes
+      else
+        formatted_nodes ++ ["... and #{total_nodes - max_nodes_per_result} more nodes.\n"]
+      end
+
+    summary_string =
+      if total_nodes == 1 do
+        "There is one node"
+      else
+        "There are #{total_nodes} nodes"
+      end
+
+    [
+      "#{summary_string} with this violation:\n\n"
+    ] ++ formatted_nodes
   end
 
   defp trim_string(string, max_length) do
@@ -57,6 +92,29 @@ defmodule A11yAudit.Formatter do
       {start, _} = String.split_at(string, max_length)
       "#{start}..."
     end
+  end
+
+  defp format_list_in_failure_summary(failure_summary) do
+    failure_summary
+    |> String.split(~r/Fix (any|all) of the following:\n(.*)\n\n/,
+      include_captures: true,
+      trim: true
+    )
+    |> Enum.map(fn maybe_list_string ->
+      maybe_list_string
+      |> String.split(~r/\n\n/, include_captures: true, trim: true)
+      |> Enum.map(fn string_chunk ->
+        if Regex.match?(~r/^Fix (any|all) of the following:\n/, string_chunk) do
+          # string chunk is a list, add dashes to list elements
+          String.replace(string_chunk, ~r/\n  ([^\s])/, "\n  - \\g{1}")
+        else
+          # string chunk does not match the expected list format, don't modify it
+          string_chunk
+        end
+      end)
+      |> Enum.join("")
+    end)
+    |> Enum.join("")
   end
 
   @doc false
--- test/a11y_audit/formatter_test.exs
+++ test/a11y_audit/formatter_test.exs
@@ -8,13 +8,12 @@ defmodule A11yAudit.FormatterTest do
         %A11yAudit.Results.Node{html: "<p>Lorem ipsum</p>"}
       ]
 
-      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("\n") ===
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
                """
-               Node 1/2
-               <h1>Hello</h1>
+               There are 2 nodes with this violation:
 
-               Node 2/2
-               <p>Lorem ipsum</p>
+               1. <h1>Hello</h1>
+               2. <p>Lorem ipsum</p>
                """
     end
 
@@ -29,23 +28,15 @@ defmodule A11yAudit.FormatterTest do
         %A11yAudit.Results.Node{html: "<p>Lorem ipsum</p>"}
       ]
 
-      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("\n") ===
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
                """
-               Node 1/7
-               <h1>Hello</h1>
+               There are 7 nodes with this violation:
 
-               Node 2/7
-               <p>Lorem ipsum</p>
-
-               Node 3/7
-               <p>Lorem ipsum</p>
-
-               Node 4/7
-               <p>Lorem ipsum</p>
-
-               Node 5/7
-               <p>Lorem ipsum</p>
-
+               1. <h1>Hello</h1>
+               2. <p>Lorem ipsum</p>
+               3. <p>Lorem ipsum</p>
+               4. <p>Lorem ipsum</p>
+               5. <p>Lorem ipsum</p>
                ... and 2 more nodes.
                """
     end
@@ -58,14 +49,112 @@ defmodule A11yAudit.FormatterTest do
         }
       ]
 
-      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("\n") ===
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
                """
-               Node 1/1
-               <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt u...
+               There is one node with this violation:
+
+               1. <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt u...
                """
     end
 
-    test "turns failure summary into a list if possible" do
+    test "turns failure summary into a list of possible actions" do
+      nodes = [
+        %A11yAudit.Results.Node{
+          html: "<p>Hello 1</p>",
+          failure_summary:
+            "Fix any of the following:\n  Element has no child that is a title\n  aria-label attribute does not exist or is empty\n  aria-labelledby attribute does not exist, references elements that do not exist or references elements that are empty\n  Element has no title attribute"
+        },
+        %A11yAudit.Results.Node{
+          html: "<p>Hello 2</p>",
+          failure_summary: "Fix all of the following:\n  Page must have a level-one heading"
+        }
+      ]
+
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
+               """
+               There are 2 nodes with this violation:
+
+               1. <p>Hello 1</p>
+
+                  Fix any of the following:
+                    - Element has no child that is a title
+                    - aria-label attribute does not exist or is empty
+                    - aria-labelledby attribute does not exist, references elements that do not exist or references elements that are empty
+                    - Element has no title attribute
+
+               2. <p>Hello 2</p>
+
+                  Fix all of the following:
+                    - Page must have a level-one heading
+
+               """
+    end
+
+    test "failure summary can include both 'all' and 'any' recommendations" do
+      nodes = [
+        %A11yAudit.Results.Node{
+          html: "<p>Hello</p>",
+          failure_summary:
+            "Fix all of the following:\n  Element is in tab order and does not have accessible text\n\nFix any of the following:\n  Element does not have text that is visible to screen readers\n  aria-label attribute does not exist or is empty\n  aria-labelledby attribute does not exist, references elements that do not exist or references elements that are empty\n  Element has no title attribute"
+        }
+      ]
+
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
+               """
+               There is one node with this violation:
+
+               1. <p>Hello</p>
+
+                  Fix all of the following:
+                    - Element is in tab order and does not have accessible text
+
+                  Fix any of the following:
+                    - Element does not have text that is visible to screen readers
+                    - aria-label attribute does not exist or is empty
+                    - aria-labelledby attribute does not exist, references elements that do not exist or references elements that are empty
+                    - Element has no title attribute
+
+               """
+    end
+
+    test "keeps failure summary unchanged if it does not match patterns" do
+      nodes = [
+        %A11yAudit.Results.Node{
+          html: "<p>Hello</p>",
+          failure_summary: "Lorem ipsum: Eeny, meeny, miny, moe."
+        }
+      ]
+
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
+               """
+               There is one node with this violation:
+
+               1. <p>Hello</p>
+
+                  Lorem ipsum: Eeny, meeny, miny, moe.
+
+               """
+
+      nodes = [
+        %A11yAudit.Results.Node{
+          html: "<p>Hello</p>",
+          failure_summary:
+            "Fix all of the following:\n  Element is in tab order and does not have accessible text\n\nLorem ipsum: Eeny, meeny, miny, moe."
+        }
+      ]
+
+      assert A11yAudit.Formatter.format_nodes(nodes) |> Enum.join("") ===
+               """
+               There is one node with this violation:
+
+               1. <p>Hello</p>
+
+                  Fix all of the following:
+                    - Element is in tab order and does not have accessible text
+
+                  Lorem ipsum: Eeny, meeny, miny, moe.
+
+               """
     end
   end
 end
