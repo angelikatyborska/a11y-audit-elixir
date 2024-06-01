4106fdb423b5cca1cff6e4ed8d47f0bbef1681bb
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-06-01 19:46:39 +0200

    Fix pluralization in assertion

--- lib/a11y_audit/assertions.ex
+++ lib/a11y_audit/assertions.ex
@@ -2,9 +2,13 @@ defmodule A11yAudit.Assertions do
   alias A11yAudit.Formatter
 
   def assert_no_violations(results) do
-    if results.violations != [] do
+    violations_count = Enum.count(results.violations)
+
+    if violations_count > 0 do
+      noun = if violations_count == 1, do: "violation", else: "violations"
+
       error_message =
-        "Expected page to have no accessibility violations, but got #{Enum.count(results.violations)} violations.\n"
+        "Expected page to have no accessibility violations, but got #{Enum.count(results.violations)} #{noun}.\n\n"
 
       error_message = error_message <> Formatter.format_results(results)
 
--- test/a11y_audit/assertions_test.exs
+++ test/a11y_audit/assertions_test.exs
@@ -14,7 +14,7 @@ defmodule A11yAudit.AssertionsTest do
              }) == nil
     end
 
-    test "some violations" do
+    test "one violation" do
       try do
         Assertions.assert_no_violations(%Results{
           url: "url",
@@ -36,9 +36,52 @@ defmodule A11yAudit.AssertionsTest do
         error in [ExUnit.AssertionError] ->
           assert error.message ==
                    """
-                   Expected page to have no accessibility violations, but got 1 violations.
+                   Expected page to have no accessibility violations, but got 1 violation.
+
                    \e[33m\e[7m moderate \e[0m help
                    \e[33m┃\e[0m Learn more: help_url
+
+                   """
+      end
+    end
+
+    test "some violations" do
+      try do
+        Assertions.assert_no_violations(%Results{
+          url: "url",
+          test_engine: "3",
+          violations: [
+            %Violation{
+              id: "id 1",
+              description: "description 1",
+              help: "help 1",
+              help_url: "help_url 1",
+              impact: :moderate,
+              nodes: []
+            },
+            %Violation{
+              id: "id 2",
+              description: "description 2",
+              help: "help 2",
+              help_url: "help_url 2",
+              impact: :serious,
+              nodes: []
+            }
+          ]
+        })
+
+        raise "this line should not be reached"
+      rescue
+        error in [ExUnit.AssertionError] ->
+          assert error.message ==
+                   """
+                   Expected page to have no accessibility violations, but got 2 violations.
+
+                   \e[31m\e[7m serious \e[0m help 2
+                   \e[31m┃\e[0m Learn more: help_url 2
+
+                   \e[33m\e[7m moderate \e[0m help 1
+                   \e[33m┃\e[0m Learn more: help_url 1
 
                    """
       end
