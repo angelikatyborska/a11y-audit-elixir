eeb4ccf43e6a2340a87bc1cc54f4378e7f4b6449
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-06-01 20:11:36 +0200

    Lower elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule A11yAudit.MixProject do
     [
       app: :a11y_audit,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
