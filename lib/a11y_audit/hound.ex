518f7896f4b79e1c33c24a2ddcc817735c83a1df
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-06-08 11:19:54 +0200

    Don't print compilation warnings about the other library

--- lib/a11y_audit/hound.ex
+++ lib/a11y_audit/hound.ex
@@ -1,34 +1,22 @@
-defmodule A11yAudit.Hound do
-  @moduledoc "ExUnit assertions for Hound tests"
+if Code.ensure_loaded?(Hound) do
+  defmodule A11yAudit.Hound do
+    @moduledoc "ExUnit assertions for Hound tests"
 
-  require Logger
+    alias A11yAudit.JS
+    alias A11yAudit.Results
+    alias A11yAudit.Assertions
 
-  alias A11yAudit.JS
-  alias A11yAudit.Results
-  alias A11yAudit.Assertions
+    @doc """
+    Runs an audit on the current page and asserts that the audit results have no violations.
 
-  @doc """
-  Runs an audit on the current page and asserts that the audit results have no violations.
-
-  See `A11yAudit.Assertions.assert_no_violations/2` for the list of configuration options.
-  """
-  @spec assert_no_violations(Assertions.opts()) :: nil | no_return
-  def assert_no_violations(opts \\ []) do
-    if Code.ensure_loaded?(Hound) do
+    See `A11yAudit.Assertions.assert_no_violations/2` for the list of configuration options.
+    """
+    @spec assert_no_violations(Assertions.opts()) :: nil | no_return
+    def assert_no_violations(opts \\ []) do
       Hound.Helpers.ScriptExecution.execute_script(JS.axe_core())
       axe_result = Hound.Helpers.ScriptExecution.execute_script(JS.await_audit_results())
       audit_results = Results.from_json(axe_result)
       Assertions.assert_no_violations(audit_results, opts)
-    else
-      Logger.error("""
-      Could not find dependency :hound.
-
-      Please add :hound to your dependencies:
-
-          {:hound, "~> 1.1"}
-      """)
-
-      raise "missing dependency :hound"
     end
   end
 end
--- lib/a11y_audit/wallaby.ex
+++ lib/a11y_audit/wallaby.ex
@@ -1,37 +1,27 @@
-defmodule A11yAudit.Wallaby do
-  @moduledoc "ExUnit assertions for Wallaby tests"
+if Code.ensure_loaded?(Wallaby) do
+  defmodule A11yAudit.Wallaby do
+    @moduledoc "ExUnit assertions for Wallaby tests"
 
-  require Logger
+    require Logger
 
-  alias A11yAudit.JS
-  alias A11yAudit.Results
-  alias A11yAudit.Assertions
+    alias A11yAudit.JS
+    alias A11yAudit.Results
+    alias A11yAudit.Assertions
 
-  @doc """
-  Runs an audit on the current page and asserts that the audit results have no violations.
+    @doc """
+    Runs an audit on the current page and asserts that the audit results have no violations.
 
-  See `A11yAudit.Assertions.assert_no_violations/2` for the list of configuration options.
-  """
-  @spec assert_no_violations(Wallaby.Browser.parent(), Assertions.opts()) ::
-          Wallaby.Browser.parent() | no_return()
-  def assert_no_violations(session, opts \\ []) do
-    if Code.ensure_loaded?(Wallaby) do
+    See `A11yAudit.Assertions.assert_no_violations/2` for the list of configuration options.
+    """
+    @spec assert_no_violations(Wallaby.Browser.parent(), Assertions.opts()) ::
+            Wallaby.Browser.parent() | no_return()
+    def assert_no_violations(session, opts \\ []) do
       session
       |> Wallaby.Browser.execute_script(JS.axe_core())
       |> Wallaby.Browser.execute_script(JS.await_audit_results(), [], fn axe_result ->
         audit_results = Results.from_json(axe_result)
         Assertions.assert_no_violations(audit_results, opts)
       end)
-    else
-      Logger.error("""
-      Could not find dependency :wallaby.
-
-      Please add :wallaby to your dependencies:
-
-          {:wallaby, "~> 0.30"}
-      """)
-
-      raise "missing dependency :wallaby"
     end
   end
 end
