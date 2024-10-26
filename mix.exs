859aa8eadd365ccaaf340681ba6ea17f704c48cf
Angelika Cathor <angelikacathor@fastmail.com>
2024-10-26 17:23:53 +0200

    v0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule A11yAudit.MixProject do
   def project do
     [
       app: :a11y_audit,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
